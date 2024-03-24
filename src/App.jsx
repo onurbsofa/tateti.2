a233e466a02b10f3891becdaf0113b3ea725bd07
BoFo <brunofazio466@gmail.com>
2024-03-24 19:35:40 -0300

    estilos basicos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import  Square  from './components/Square'
+
 export default function App () {
   return (
     <>
       <main>
         <h1>Segunda Version Ta Te Ti</h1>
+        <div className="board-row">
+          <Square />
+          <Square />
+          <Square />
+        </div>
       </main>
     </>
   )
